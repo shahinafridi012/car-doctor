e9dd81e1ad3130d1a061c72c290cbab27b62e22d
unknown <shahinafridi012@gmail.com>
2024-08-02 17:17:44 +0530

    first commit

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,52 @@
+
+import img from '../../../assets/DTW-coveralls-black_F22_longsleeve_shortsleeve_Faye-Hadley_lifestyle_0363_2048x2048.webp'
+import img_1 from '../../../assets/220721175751-woman-mechanic-stock.jpg'
+import img_2 from '../../../assets/valuing-an-automotive-repair-shop.jpg'
+import img_3 from '../../../assets/car-repair-costs-hero_1_1.webp'
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img
+                        src={img}
+                        className="w-full" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img
+                        src={img_1}
+                        className="w-full" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img
+                        src={img_2}
+                        className="w-full" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img
+                        src={img_3}
+                        className="w-full" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+import logo from '../../../assets/Screenshot_2024-08-02_160506-removebg-preview.png'
+
+const NavBar = () => {
+    const link = <>
+    
+    <li><Link to='/'>Home</Link></li>
+    <li><Link to='/about'>About</Link></li>
+    </>
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                           {link}
+                          
+                        </ul>
+                    </div>
+                    <Link to='/' className=" text-center text-red-700"><img className=" w-[150px] lg:w-[200px] text-cen' text-red-800 " src={logo} alt="" /></Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                       {link}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Button</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
